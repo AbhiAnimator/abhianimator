4582b57ab011d01d0ce45b7a320dec65f4a7865d
danny <agarwaldanny@gmail.com>
2023-07-05 22:28:42 +0530

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,3 @@ function openNav() {
 function closeNav() {
    	document.getElementById('navigation').style.display = 'none';
   }
-
-  var menuItems = document.querySelectorAll('#navigation a');
-  menuItems.forEach(function (menuItem) {
-    menuItem.addEventListener('click', function () {
-      closeNav();
-    });
-  });
-  // function openNav() {
-  //   document.getElementById("navigation").style.width = "100%";
-  //   document.body.classList.add("mobile-open"); // Add the class to body element
-  // }
-  
-  // function closeNav() {
-  //   document.getElementById("navigation").style.width = "0";
-  //   document.body.classList.remove("mobile-open"); // Remove the class from body element
-  // }
